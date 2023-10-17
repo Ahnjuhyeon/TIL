53e97f70e08adc71b4e806c5bf23cfb998b64a00
sgm4247 <sgm4247@naver.com>
2023-10-18 02:32:31 +0900

    20231018

--- 20231016/src/page/index.jsx
+++ 20231016/src/page/index.jsx
@@ -3,8 +3,12 @@ import { GetSearch } from "../apis/search.api";
 
 const SearchInput = () => {
   const [state, setState] = useState([]); //배열..
+  const [recentSearch, setRecentSearch] = useState([]);
+  const [searchValue, setSearchValue] = useState(""); // 추가: 입력된 검색어를 저장할 상태
+
   const onChangeSearch = async (e) => {
     e.preventDefault();
+    setSearchValue(e.target.value); // 검색어 입력값 업데이트 /이걸 써야 값이 화면에 보임
     try {
       const data = await GetSearch(e.target.value);
       // console.log(data);
@@ -13,10 +17,29 @@ const SearchInput = () => {
       setState([err.response.data]);
     }
   };
+
+  const onClickSearch = () => {
+    if (recentSearch.length >= 5) {
+      // 검색어가 비어있지 않을 때만 추가
+      recentSearch.pop();
+      setRecentSearch([searchValue, ...recentSearch]);
+    } else {
+      setRecentSearch([searchValue, ...recentSearch]);
+    }
+  };
+
   return (
     <>
       <input onChange={onChangeSearch} />
-      <button>search</button>
+      <button onClick={onClickSearch}>search</button>
+      <p>최근 검색어</p>
+      <ul>
+        {recentSearch.map((data, index) => (
+          <li key={index}>{data}</li>
+        ))}
+      </ul>
+      <hr />
+      <p>추천 검색어</p>
       <ul>
         {state.map((data, index) => (
           <li key={index}>{data}</li>
@@ -26,3 +49,10 @@ const SearchInput = () => {
   );
 };
 export default SearchInput;
+
+//최근검색어
+/*
+최근 검색 에 이전에 기록이 나와야함..
+1. 일단 최근 검색어에 내가 검색한게 클릭 시 보여져야함 
+
+*/
